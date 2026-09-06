c58fae4c9fdd46328800499e15e736ba3a60af99
Rahulvgosavi <86643301+Rahulvgosavi@users.noreply.github.com>
2026-09-06 12:06:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__)
+DOWNLOAD_FOLDER = 'downloads'
+
+@app.route('/downloads/<filename>')
+def download_file(filename):
+    return send_from_directory(DOWNLOAD_FOLDER, filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
